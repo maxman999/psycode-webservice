864df02ab819743d69eb26738b26105d65202bb1
maxman999 <gom_55@naver.com>
2021-09-07 16:40:34 +0900

    210907: 게시글 페이징 기능 구현 및 버그 수정

--- src/test/java/com/kjy/myapp/springboot/domian/posts/PostsRepositoryTest.java
+++ src/test/java/com/kjy/myapp/springboot/domian/posts/PostsRepositoryTest.java
@@ -2,7 +2,6 @@ package com.kjy.myapp.springboot.domian.posts;
 
 import com.kjy.myapp.springboot.domain.posts.Posts;
 import com.kjy.myapp.springboot.domain.posts.PostsRepository;
-import javafx.geometry.Pos;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
