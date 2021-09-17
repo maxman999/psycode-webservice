376c4ccdec2ffc7d6b0e1bd8988e97f844cafa36
maxman999 <gom_55@naver.com>
2021-09-17 17:53:52 +0900

    210917: 뉴스 조회 기능 작성

--- src/main/resources/static/js/app/news.js
+++ src/main/resources/static/js/app/news.js
@@ -11,23 +11,29 @@ $(document).ready(function(){
 			let news = JSON.parse(result).items;
 			let str = "";
 			for (var i = 0; i < news.length; i++) {
-				str += "<tr id = 'tr-scrap' class='odd gradeX' ";
-				str += " data-title = '"+news[i].title+"' ";
-				str += " data-description = '"+news[i].description+"' ";
-				str += " data-originallink = '"+news[i].originallink+"' ";
-				str += " data-pubDate = '"+news[i].pubDate+"' ";
-				str += ">";
-				str += "<td>" + news[i].title + "</td>";
-				str += "<td>" + news[i].description + "</td>";
-				str += "<td>" + news[i].originallink + "</td>";
-				str += "<td>" + news[i].pubDate + "</td>";
-				str += "<td><button class = 'btn btn-default'>scrap</button></td>";
-				str += "</tr>";
+				str += "<a href='" + news[i].originallink + "' id = 'news-li' class='list-group-item d-flex justify-content-between align-items-start list-group-item-action'>";
+				str += "<div class='ms-2 me-auto'>";
+				str += "<div class='fw-bold mb-1'>" + news[i].title +"</div>";
+                str += "<div id='div-desc'>" + news[i].description + "</div>";
+                str += "</div>"
+                str += "<div id = btn-scrap><button class = 'btn btn-sm btn-outline-secondary'><i class='fas fa-pencil-alt'></i></button></div>"
+				str += "</a>";
 			}
-			$(".news-table").html(str);
+			$("#ol-news").html(str);
 		},
 		error : function(e) {
 			alert("통신 실패");
 		}
 	})
 })
+
+//str += " data-title = '"+news[i].title+"' ";
+//				str += " data-description = '"+news[i].description+"' ";
+//				str += " data-originallink = '"+news[i].originallink+"' ";
+//				str += " data-pubDate = '"+news[i].pubDate+"' ";
+//				str += ">";
+//				str += "<td>" + news[i].title + "</td>";
+//				str += "<td>" + news[i].description + "</td>";
+//				str += "<td>" + news[i].originallink + "</td>";
+//				str += "<td>" + news[i].pubDate + "</td>";
+//				str += "<td><button class = 'btn btn-default'>scrap</button></td>";
